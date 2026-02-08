aa82bed41cd496e4c97d5901b40a74b81980d40a
puneet <ypunit168@gmail.com>
2026-02-08 20:20:48 +0530

    denied page addeed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import Signup from './pages/Signup';
 import Login from './pages/Login';
 import CourseList from './pages/course/CourseList';
 import Contact from './pages/Contact';
+import Denied from './pages/Denied';
 // import Footer from './components/Footer'
 
 function App() {
@@ -20,7 +21,7 @@ function App() {
         <Route path="/login" element={ <Login /> }></Route>
         <Route path="/courses" element={ <CourseList /> }></Route>
         <Route path="/contact" element={ <Contact /> }></Route>
-
+        <Route path="/denied" element={ <Denied /> }></Route>
 
         <Route path="*" element={ <NotFound /> }></Route>
 
