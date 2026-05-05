dda783c56ea5e75d1e046a5ebc3f63ddc6730f22
PuneetYadav <138369826+Puneetwhy@users.noreply.github.com>
2026-05-05 11:37:15 +0530

    Update axiosInstance.js

--- client/src/helpers/axiosInstance.js
+++ client/src/helpers/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://lms-e-backend-s3rw.onrender.com",
+  baseURL: "https://lms-e-backend-s3rw.onrender.com/api/v1",
   withCredentials: true,
 });
 
