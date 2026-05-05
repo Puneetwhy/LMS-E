4b0aa6e7c8d49918a7d55079210740c766296db3
PuneetYadav <138369826+Puneetwhy@users.noreply.github.com>
2026-05-05 11:21:21 +0530

    Update axios base URL to production endpoint

--- client/src/helpers/axiosInstance.js
+++ client/src/helpers/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5014",
+  baseURL: "https://lms-e-api.onrender.com",
   withCredentials: true,
 });
 
