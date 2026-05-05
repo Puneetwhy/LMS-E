8cf023cbaad833cc6809d7d54f3efb0655bb0d6a
PuneetYadav <138369826+Puneetwhy@users.noreply.github.com>
2026-05-05 11:31:28 +0530

    Update axios base URL for API requests

--- client/src/helpers/axiosInstance.js
+++ client/src/helpers/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://lms-e-api.onrender.com",
+  baseURL: "https://lms-e-backend-s3rw.onrender.com",
   withCredentials: true,
 });
 
