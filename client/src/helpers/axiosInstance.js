d3478360aeb025825737d9068dd42b4bd447d3e7
PuneetYadav <138369826+Puneetwhy@users.noreply.github.com>
2026-05-05 12:59:08 +0530

    Update axiosInstance.js

--- client/src/helpers/axiosInstance.js
+++ client/src/helpers/axiosInstance.js
@@ -1,8 +1,18 @@
+// axiosInstance.js
 import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL: "https://lms-e-backend-s3rw.onrender.com",
   withCredentials: true,
+});
+
+// Attach token from localStorage as fallback
+axiosInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token"); // or wherever you store it
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
 
 export default axiosInstance;
